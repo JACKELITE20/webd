ddfc1231fece322e2038a6fe953aad56cac1e44f
JACKELITE20 <158245111+JACKELITE20@users.noreply.github.com>
2024-04-03 09:42:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 showSection('home');
 
 function showSection(sectionId) {
-  // Hide all sections
   document.getElementById('home').style.display = 'none';
   document.getElementById('about').style.display = 'none';
   document.getElementById('skills').style.display = 'none';
@@ -9,6 +8,5 @@ function showSection(sectionId) {
   document.getElementById('projects').style.display = 'none';
 
 
-  // Show the selected section
   document.getElementById(sectionId).style.display = 'block';
 }
